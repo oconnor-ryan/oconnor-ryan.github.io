a195efe14d6ee9b492f26b0c10132c46017ea8c1
oconnor-ryan <oconnor.ryan.m@proton.me>
2023-05-30 12:22:01 -0500

    Added navbar so that it displays links on wide screen and a hamburger menu on small screens.

--- src/components/top-navbar.tsx
+++ src/components/top-navbar.tsx
@@ -1,9 +1,25 @@
 import styles from "./top-navbar-styles.module.scss";
 
 export default function TopNavbar()  {
-    return (
-        <header className={styles.header}>
-            <h1>Navbar</h1>
-        </header>
-    );
+  return (
+    <header className={styles.header}>
+      <a href="/" className={styles["logo-container"]}>
+        <h1 className={styles.name}>Ryan O'Connor</h1>
+      </a>
+
+      <div className={styles["nav-wrapper"]}>
+        <div className={styles.hamburger}>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <nav className={styles.nav}>
+          <a href="/blog">Blog</a>
+          <a href="/blog">GitHub</a>
+          <a href="/blog">Experience</a>
+        </nav>
+      </div>
+      
+    </header>
+  );
 }
