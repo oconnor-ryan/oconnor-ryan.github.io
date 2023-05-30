d92764f67e8f8232fc79a381c992bf668bd823e2
oconnor-ryan <oconnor.ryan.m@proton.me>
2023-05-30 18:12:08 -0500

    Added socials to navbar.

--- src/components/top-navbar.tsx
+++ src/components/top-navbar.tsx
@@ -3,6 +3,13 @@ import styles from "./top-navbar-styles.module.scss";
 export default function TopNavbar()  {
   return (
     <header className={styles.header}>
+      <div className={styles["social-container"]}>
+        {/* target="_blank" tells browser to open link in new tab. */}
+        <a href="mailto:oconnor.ryan.m@proton.me"><img src="iconmonstr-email-10.svg" /></a>
+        <a href="https://github.com/oconnor-ryan" target="_blank"><img src="/iconmonstr-github-4.svg" /></a>
+        <a href="https://www.linkedin.com/in/ryan-o-connor-5615b326a/" target="_blank"><img src="/iconmonstr-linkedin-4.svg" /></a>
+      </div>
+      
       <a href="/" className={styles["logo-container"]}>
         <h1 className={styles.name}>Ryan O'Connor</h1>
       </a>
@@ -14,9 +21,10 @@ export default function TopNavbar()  {
           <div></div>
         </div>
         <nav className={styles.nav}>
+          <a href="/#about">About</a>
+          <a href="/#education">Education</a>
+          <a href="/#experience">Experience</a>
           <a href="/blog">Blog</a>
-          <a href="/blog">GitHub</a>
-          <a href="/blog">Experience</a>
         </nav>
       </div>
       
