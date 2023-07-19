2c4db29c67926efd297b78ed8f3a334ad6cdaf7f
oconnor-ryan <oconnor.ryan.m@proton.me>
2023-07-19 16:19:27 -0500

    Changed size of borders in Frame component.

--- src/components/frame.tsx
+++ src/components/frame.tsx
@@ -1,10 +1,7 @@
 import styles from "./frame.module.scss";
 
-export default function Frame({src, caption, href, newTab, borderSize} : {src: string, caption?: string | undefined, href?: string | undefined, newTab?: boolean | undefined, borderSize?: number | undefined}) {
-  if(!borderSize) {
-    borderSize = 20;
-  }
 
+export default function Frame({src, caption, href, newTab} : {src: string, caption?: string | undefined, href?: string | undefined, newTab?: boolean | undefined}) {
   let frameImage = href ? 
     <a href={href} target={newTab ? "_blank" : ""} className={styles.pictureContainer}>
       <img className={styles.picture} src={src} alt={caption}></img>
