29a25797b19f851c3c62bef50a5a06237fb6f92a
oconnor-ryan <oconnor.ryan.m@proton.me>
2023-07-26 01:32:53 -0500

    Added site map and a back to top button on the footer. Also created a fallback version of the back to top button when the user has Javascript disabled.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,17 +1,21 @@
+import Script from "next/script";
 import styles from "./footer-styles.module.scss";
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
+      <a className={`${styles.backToTop} ${styles.noJS}`} href="./#main-page-wrapper">&#8593;</a>
+      <button className={`${styles.backToTop} ${styles.js}`}>&#8593;</button>
+
       <div className={styles.contentWrapper}>
         <div id="contact-info" className={styles.contactInfo}>
-          <h1>Contact Info</h1>
+          <h2>Contact Info</h2>
           <ul className={styles["social-container-footer"]}>
             {/* target="_blank" tells browser to open link in new tab. */}
             <li>
               <a href="mailto:oconnor.ryan.m@proton.me">
                 <div className={`${styles.icons} ${styles.iconEmail}`}></div>
-                <div className={styles.iconDesc}>oconnor.ryan.m@proton.me</div>
+                <div className={styles.iconDesc}>Email</div>
               </a>
             </li>
             <li>
@@ -29,13 +33,47 @@ export default function Footer() {
           </ul>
         </div>
 
+        <hr/>
+
+        <div className={styles.sitemap}>
+          <h2>Site Map</h2>
+          <ul className={styles.links}>
+            <li><a href="/blog" className={styles.title}>Blog</a></li>
+            <li><a href="/woodgallery" className={styles.title}>Wood Gallery</a></li>
+          </ul>
+        </div>
+
+        <hr/>
+
         <div className={styles.nextjs}>
           <p>This website was made using:</p>
           <a href="https://nextjs.org"><div className={styles.nextjsLogo}></div></a>
         </div>
+
+        <hr/>
       </div>
 
       <div className={styles.copyright}>&copy; Ryan O'Connor 2023</div>
+      <Script 
+        id="smooth-scroll"
+        dangerouslySetInnerHTML={{
+          __html:`
+            const backToTop = document.getElementsByClassName("${styles.backToTop} ${styles.js}")[0];
+            const backToTopNoJS = document.getElementsByClassName("${styles.backToTop} ${styles.noJS}")[0];
+
+            backToTopNoJS.style.display = 'none'; //remove the noJS version of the backToTop button here
+            backToTop.style.display = 'block'; //make the JS version of the backToTop button visible
+
+            //use smooth scrolling to move to top of window when pressing the button
+            backToTop.addEventListener('click', (ev) => {
+              window.scrollTo({
+                top: 0,
+                behavior: "smooth"
+              });
+            });
+          `
+        }}
+      />
     </footer>
   )
 }
