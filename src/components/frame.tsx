0b436e7e5397e9632b4389af7e46ccdde3aed660
oconnor-ryan <oconnor.ryan.m@proton.me>
2023-07-25 22:57:05 -0500

    Moved the ModalImage code from the Frame component to the ModalImage component and gave ModalImage more parameters that allow it to be used with ANY component.

--- src/components/frame.tsx
+++ src/components/frame.tsx
@@ -1,9 +1,7 @@
 import { CSSProperties } from "react";
 import styles from "./frame.module.scss";
 
-import modalStyles from "./modal-image.module.scss";
 import ModalImage from "./modal-image";
-import Script from "next/script";
 
 
 export default function Frame({src, caption, href, newTab, useModal, css} : {
@@ -24,81 +22,40 @@ export default function Frame({src, caption, href, newTab, useModal, css} : {
     useModal = false;
   }
 
-  let frameImage = href ? 
-    <a href={href} target={newTab ? "_blank" : ""} className={styles.pictureContainer}>
+  //if href is undefined and useModal is false, use normal div as container for image
+  let picContainer =
+    <div className={styles.pictureContainer}>
       <img className={styles.picture} src={src} alt={caption}></img>
-    </a> 
-    : //if useModal is true, turn the cursor into a pointer when this element is hovered over
-    <div style={{cursor: useModal ? 'pointer' : undefined}} className={styles.pictureContainer}>
-      <img className={styles.picture} src={src} alt={caption}></img>
-    </div>;
+    </div>
+  ;
 
-  //This inserts a modal image inside the Frame component and 
-  //injects a script that will add onClick event listeners to each Frame with a ModalImage component
-  //that check for when the picture is clicked. When the picture is clicked, the ModalImage will
-  //cover the entire screen and make it easier to view the image in the Frame component.
-  let modal = useModal ? 
-  <>
-    <ModalImage imgSrc={src} caption={caption ? caption : ""}/>
-    <Script id="open_modal" dangerouslySetInnerHTML={{
-      __html: `
-
-        /*
-          Because this script is only injected once no matter how many Frame components there are,
-          EVERY Frame component must be retrieved and given event listeners when appropriate
-          (when a Frame has a ModalImage component)
-        */
-        const frames = document.getElementsByClassName("${styles.frame}");
-
-        for(let frame of frames) {
-          const modal = frame.getElementsByClassName("${modalStyles.modal}")[0];
-          const closeButton = modal.getElementsByClassName("${modalStyles.close}")[0];
-
-          //if a frame does not have a modal, ignore it.
-          if(modal === undefined) {
-            continue;
-          }
-
-          frame.addEventListener('click', (ev) => {
-            ev.stopPropagation(); 
-
-            if(!modal.classList.contains("${modalStyles.visible}")) {
-              modal.classList.add("${modalStyles.visible}");
-              document.body.classList.add("no-scroll"); //dont let the webpage scroll behind the modal 
-            }
-          });
+  //use an anchor as a wrapper for the image
+  if(href) {
+    picContainer = 
+      <a href={href} target={newTab ? "_blank" : ""} className={styles.pictureContainer}>
+        <img className={styles.picture} src={src} alt={caption}></img>
+      </a>
+    ;
+  } 
   
-          closeButton.addEventListener('click', (ev) => {
-            //prevents issue where when close button is clicked, the 
-            //event gets propagated to any parent elements, including the frame.
-            //This means that if you press the X button, the modal's 'visible' class is
-            //removed, then the frame's event listener is called immediately after this,
-            //inserting the 'visible' class back into the modal's classList
-
-            ev.stopPropagation(); 
-
-            if(modal.classList.contains("${modalStyles.visible}")) {
-              modal.classList.remove("${modalStyles.visible}");
-              document.body.classList.remove("no-scroll"); //dont let the webpage scroll behind the modal 
-
-            }
-            
-          });
-
-        }
-        
-      `
-    }}/>
-  </>
-  : //if not using model, put nothing here
-  <></>;
-
+  //use a modal image component as a wrapper for the image
+  else if(useModal) {
+    picContainer = 
+      <ModalImage 
+        className={styles.pictureContainer} 
+        imgSrc={src} 
+        caption={caption ? caption : ""} 
+        style={{cursor: 'pointer'}}
+      >
+        <img className={styles.picture} src={src} alt={caption}></img>
+      </ModalImage>
+    ;
+  }
 
   return (
     <div style={css} className={styles.frame}>
-      {frameImage}
+      {picContainer}
       {caption ? <p className={styles.caption}>{caption}</p> : null}
-      {modal}
     </div>
     
   )
--- src/components/modal-image.tsx
+++ src/components/modal-image.tsx
@@ -1,9 +1,71 @@
+import Script from "next/script";
 import styles from "./modal-image.module.scss";
+import { CSSProperties, ReactNode } from "react";
 
-export default function ModalImage({imgSrc, caption} : {imgSrc: string, caption: string}) {
-  return <div className={styles.modal}>
-    <span className={styles.close}>&times;</span>
-    <img className={styles.image} src={imgSrc}></img>
-    <div className={styles.caption}>{caption}</div>
+/*
+  The ModalImage component will display a popup of the image specified by the imgSrc parameter
+  when the user clicks inside the ModalImage or its children. 
+  
+  The Modal Image is NOT THE MODAL BY ITSELF,
+  it is a wrapper that contains both the popup that displays the image over the entire screen and 
+  a list of children that it displays when the popup is not visible.
+*/
+export default function ModalImage ({imgSrc, caption, className, style, children} : {
+  imgSrc: string, 
+  caption: string, 
+  className?: string, 
+  style?: CSSProperties, 
+  children: ReactNode 
+}) {
+
+  //allow style to be modified and allow classNames to be appended to the root div's className
+  return <div style={style} className={`${styles.modal} ${className ? className : ""}`}>
+    <div className={styles.popup}>
+      <span className={styles.close}>&times;</span>
+      <img className={styles.image} src={imgSrc}></img>
+      <div className={styles.caption}>{caption}</div>
+    </div>
+    
+    <div className={styles.wrappedContent}>{children}</div>
+
+    <Script id="modal_handler" dangerouslySetInnerHTML={{
+      __html: `
+
+        /*
+          Because this script is only injected once no matter how many ModalImage components there are,
+          EVERY ModalImage component must be retrieved and given event listeners.
+        */
+        const modals = document.getElementsByClassName("${styles.modal}");
+
+        for(let modal of modals) {
+          const closeButton = modal.getElementsByClassName("${styles.close}")[0];
+
+          modal.addEventListener('click', (ev) => {
+            ev.stopPropagation(); 
+
+            if(!modal.classList.contains("${styles.visible}")) {
+              modal.classList.add("${styles.visible}");
+              document.body.classList.add("no-scroll"); //dont let the webpage scroll behind the modal 
+            }
+          });
+  
+          closeButton.addEventListener('click', (ev) => {
+            //prevents issue where when close button is clicked, the 
+            //event gets propagated to any parent elements, including the frame.
+            //This means that if you press the X button, the modal's 'visible' class is
+            //removed, then the frame's event listener is called immediately after this,
+            //inserting the 'visible' class back into the modal's classList
+
+            ev.stopPropagation(); 
+
+            if(modal.classList.contains("${styles.visible}")) {
+              modal.classList.remove("${styles.visible}");
+              document.body.classList.remove("no-scroll"); //dont let the webpage scroll behind the modal 
+
+            }
+          });
+        }
+      `
+    }}/>
   </div>
 }
